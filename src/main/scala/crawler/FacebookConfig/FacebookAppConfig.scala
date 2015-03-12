5592a73c2b681997c44db906eccf43d9d2b8511f
Aranir <rogerkueng@gmail.com>
2015-03-12 15:49:39 +0100

    Removing sensitive outdated information

--- src/main/scala/crawler/FacebookConfig/FacebookAppConfig.scala
+++ src/main/scala/crawler/FacebookConfig/FacebookAppConfig.scala
@@ -16,10 +16,7 @@ case class FacebookAppConfig(app_id:String, app_secret:String)
 object FacebookServiceConfig {
   implicit val formats = DefaultFormats
   val facebookStorageFolder = "facebookStorage"
-  implicit val appID = envOrElse("FACEBOOK_APP_ID_REMINISCE_ME", "646896158757620")
-  implicit val appSecret = envOrElse("FACEBOOK_APP_SECRET_REMINISCE_ME", "e480504686921c24d21e58ccf8f24925")
   val facebookHostAddress = "https://graph.facebook.com"
   val maxOffset = 10
   implicit val timeout = Timeout(10 seconds)
-
 }
