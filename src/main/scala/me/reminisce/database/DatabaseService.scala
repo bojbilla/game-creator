b33bdeb1218f779cf2cb75ffeba30d2b62698ed8
Jérémy Rabasco <rabasco.jeremy@gmail.com>
2015-09-17 17:51:58 +0200

    Fixes an NPE while logging.

--- src/main/scala/me/reminisce/database/DatabaseService.scala
+++ src/main/scala/me/reminisce/database/DatabaseService.scala
@@ -1,8 +1,11 @@
 package me.reminisce.database
 
 import akka.actor.{Actor, ActorContext, ActorLogging}
+import akka.event.Logging
 
 trait DatabaseService extends Actor with ActorLogging {
   def actorRefFactory: ActorContext = context
+
+  override val log = Logging(context.system, this)
 
 }
